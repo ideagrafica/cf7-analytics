e1a3ec2ed630c9eb418802a51ee1122613b06e2f
Marco De Sangro <ideagrafica.tar@gmail.com>
2018-04-13 01:10:22 +0200

    Create universal-analytics.php

---
+++ universal-analytics.php
@@ -0,0 +1,19 @@
+function add_this_script_footer(){
+    ?>
+    <?php global $page;
+	    $page = get_the_title();
+	?>
+        <script>
+        
+            var pathname = window.location.host;
+            
+            document.addEventListener( 'wpcf7mailsent', function( event ) {
+                gtag('event', 'Richiesta Informazioni', {
+				  'event_category': 'Pagina',
+				  'event_label': '<?php echo get_the_title();?> - <?php if ( defined( 'ICL_LANGUAGE_NAME' ) ) { echo ICL_LANGUAGE_NAME; } ?>'
+				});
+            }, false );
+        
+        </script>
+<?php
+}
